76ed9b1c8fa69a87723d242b155bc020d8f21be6
Kelly Mccrow-young <932413853@cit3-501w-22.auckland.natcoll.ac.nz>
2019-07-25 15:15:06 +1200

    Setup React project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { nodeInternals } from 'stack-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.notes = [
+      {
+        id: 1,
+        text: 'watch youtube',
+      },
+      {
+        id: 2,
+        text: 'cook dinner',
+      },
+      {
+        id: 3,
+        text: 'react',
+      },
+      {
+        id: 4,
+        text: 'more react',
+      }
+    ];
+  }
+
+  render(){
+    return (
+      <div className="wrap">
+        <div className="container">
+          <div className="notes">
+
+            {
+              this.notes.map(function(note){
+
+                return (
+                  <div className="note" key={note.id}>
+                    <div className="note-body">
+                      <i className="far fa-times-circle note-remove"></i>
+                      <div className="note-text">
+                        {note.text}
+                      </div>
+                    </div>
+                  </div>
+                );
+
+              })
+            }
+       
+            <div className="note new-note">
+
+              <form className="note-body">
+        
+                  <div className="form-group">
+                    <label for="note-input">New note</label>
+                    <input type="text" className="form-control" id="note-input"/>
+                  </div>
+            
+                  <button type="submit" className="btn btn-primary">Add</button>
+              </form>
+              
+            </div>
+          </div>
+        </div>
+	    </div>
+    );
+  }
 }
 
 export default App;
